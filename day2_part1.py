51baa6db5ac0848efdc6598ab64430f8104ac99e
Aliya Bannayeva <84685008+bannayeva@users.noreply.github.com>
2023-12-03 00:34:32 +0100

    Update day2_part1.py

--- day2_part1.py
+++ day2_part1.py
@@ -19,40 +19,34 @@ for k,v in cube_dict.items():
     
 
     for draw in v:
-        #print(draw.split(','))
-        print(k)
         blue = 0
         red=0
         green =0
         check = False
-        #print(re.findall("(\d) (?:blue|red|green)",draw))
         if 'blue' in draw:
             blue+= int(''.join(re.findall("(\d\d|\d) blue",draw)))
-            print("Blue: ", blue)
             if blue >14:
                 blue_ok = False
         if 'red' in draw:
             red+= int(''.join(re.findall("(\d\d|\d) red",draw)))
-            print("Red: ",red)
             if red > 12:
                 red_ok = False
         if 'green' in draw:
             green+= int(''.join(re.findall("(\d\d|\d) green",draw)))
-            print(green)
+
             if green > 13:
                 green_ok = False
         check += blue_ok and green_ok and red_ok
-        print("check: ",check)
 
 
     if check:
         sum_of_ids+=int(k.split(' ')[1])
 
-        #print(int(k.split(' ')[1]))
+
     print("Sum: ",sum_of_ids)
 
 
-    #if blue <= 14:
+
 
 
 
